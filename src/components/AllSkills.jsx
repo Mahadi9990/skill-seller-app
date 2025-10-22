1aaafee704b5af7307e33d8e188b398d46d0612e
Mahadi9990 <mdshafayat9999@gmail.com>
2025-10-22 16:26:07 +0600

    set catagory ui

--- src/components/AllSkills.jsx
+++ src/components/AllSkills.jsx
@@ -1,9 +1,23 @@
-import React from "react";
-import { Link, useLoaderData } from "react-router";
+import React, { useEffect, useState } from "react";
+import { Link, NavLink, useLoaderData } from "react-router";
 export default function AllSkills() {
+  const [catagory, setcatagory] = useState([]);
   const data = useLoaderData();
+  useEffect(() => {
+    fetch("/Catagory.json") 
+      .then((res) => res.json())
+      .then((data) => setcatagory(data))
+      .catch((err) => console.error(err));
+  }, []);
   return (
     <div>
+      <div className="">
+        <div className="flex justify-center items-center gap-4 p-4">
+          {catagory.map((item)=> <div key={item.categoryId}>
+            <NavLink className='btn'>{item.categoryName}</NavLink>
+        </div>)}
+        </div>
+      </div>
       <div className="grid gap-4 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
         {data.map((item) => (
           <Link  key={item.skillId} to={`/skills/${item.skillId}`}>
